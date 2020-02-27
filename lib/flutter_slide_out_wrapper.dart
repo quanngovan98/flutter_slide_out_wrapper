24e325ef1dbf610509329f175087f5587469dc12
ngovanquan <quannv@sendo.vn>
2020-02-27 14:44:42 +0700

    add willAnimate

--- lib/flutter_slide_out_wrapper.dart
+++ lib/flutter_slide_out_wrapper.dart
@@ -6,12 +6,14 @@ class AutoSlideOutAnimationWrapper extends StatefulWidget {
   final Widget child;
   final int duration; //milliSeconds
   final bool isFromLeftToRight;
+  final bool willAnimate;
 
   const AutoSlideOutAnimationWrapper(
       {Key key,
       @required this.child,
       this.duration,
-      this.isFromLeftToRight = false})
+      this.isFromLeftToRight = false,
+      this.willAnimate = true})
       : super(key: key);
   @override
   _AutoSlideOutAnimationWrapperState createState() =>
@@ -30,48 +32,55 @@ class _AutoSlideOutAnimationWrapperState
   @override
   void initState() {
     super.initState();
-    slideController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: widget.duration));
-    slideAnimation =
-        Tween<double>(begin: 0, end: widget.isFromLeftToRight ? 1 : -1)
-            .animate(slideController);
+    if (widget.willAnimate) {
+      slideController = AnimationController(
+          vsync: this, duration: Duration(milliseconds: widget.duration));
+      slideAnimation =
+          Tween<double>(begin: 0, end: widget.isFromLeftToRight ? 1 : -1)
+              .animate(slideController);
 
-    scaleController = AnimationController(
-        vsync: this, duration: Duration(milliseconds: widget.duration));
-    scaleAnimation = Tween<double>(begin: 1, end: 0).animate(scaleController);
+      scaleController = AnimationController(
+          vsync: this, duration: Duration(milliseconds: widget.duration));
+      scaleAnimation = Tween<double>(begin: 1, end: 0).animate(scaleController);
 
-    scaleController.forward();
-    slideController.forward();
-    Future.delayed(Duration(milliseconds: widget.duration), () {
-      setState(() {
-        isAnimated = true;
+      scaleController.forward();
+      slideController.forward();
+      Future.delayed(Duration(milliseconds: widget.duration), () {
+        setState(() {
+          isAnimated = true;
+        });
       });
-    });
+    }
   }
 
   @override
   void dispose() {
-    slideController.dispose();
-    scaleController.dispose();
+    if (widget.willAnimate) {
+      slideController.dispose();
+      scaleController.dispose();
+    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: slideAnimation,
-      child: SizeTransition(
-          axisAlignment: 1.0,
-          sizeFactor: scaleAnimation,
-          child: isAnimated ? Container() : widget.child),
-      builder: (context, child) {
-        return Container(
-          transform: Matrix4.identity()
-            ..translate(
-                slideAnimation.value * MediaQuery.of(context).size.width, 0),
-          child: child,
-        );
-      },
-    );
+    return widget.willAnimate
+        ? AnimatedBuilder(
+            animation: slideAnimation,
+            child: SizeTransition(
+                axisAlignment: 1.0,
+                sizeFactor: scaleAnimation,
+                child: isAnimated ? Container() : widget.child),
+            builder: (context, child) {
+              return Container(
+                transform: Matrix4.identity()
+                  ..translate(
+                      slideAnimation.value * MediaQuery.of(context).size.width,
+                      0),
+                child: child,
+              );
+            },
+          )
+        : widget.child;
   }
 }
